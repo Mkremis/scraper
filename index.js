188c40374223660f96c737ca902a1a29e31a5aa1
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-05-22 23:08:20 +0000

    getImageSize

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 import fs from 'fs-extra';
 import axios from 'axios';
+import { getImageSize } from './getImageSize.js';
+import { log, time } from './log.js';
 
+const endTime = time(' Done in');
 const {writeJSON} = fs;
 
 const INITIAL_ID_XKCD_COMICS = 2500;
 const MAX_ID_XKCD_COMICS = 2588;
 for (let id = INITIAL_ID_XKCD_COMICS; id < MAX_ID_XKCD_COMICS; id++) {
   let url = `https://xkcd.com/${id}/info.0.json`;
+  log(`Fetching ${url}...`)
   const { data } = await axios.get(url);
-  const {num, news, transcript,...restOfComic} = data;
-  const comicToStore = {id, ...restOfComic}
-  await writeJSON(`./comics/${id}`, comicToStore)
+  const {num, news, transcript, img, ...restOfComic} = data;
+  log(`Fetched comic #${num}. Getting image dimensions...`)
+  const { height, width } = await getImageSize({url:img});
+  log(`Got image dimensions: ${width}x${height}`)
+  const comicToStore = {id, img, height, width, ...restOfComic}
+  const jsonFile = `./comics/${id}`;
+  await writeJSON(jsonFile, comicToStore);
+  log(`Wrote ${jsonFile}! ✔\n`);
 }
+endTime();
