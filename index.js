d0cd5b2c01d2f65fb402d81205194e059699e48e
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-05-22 00:02:15 +0000

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+import fs from 'fs-extra';
 import axios from 'axios';
 
+const {writeJSON} = fs;
+
 const INITIAL_ID_XKCD_COMICS = 2500;
-const MAX_ID_XKCD_COMICS = 2501;
-for (let id = INITIAL_ID_XKCD_COMICS; i < MAX_ID_XKCD_COMICS; id++) {
+const MAX_ID_XKCD_COMICS = 2505;
+for (let id = INITIAL_ID_XKCD_COMICS; id < MAX_ID_XKCD_COMICS; id++) {
   let url = `https://xkcd.com/${id}/info.0.json`;
   const { data } = await axios.get(url);
-  console.log(data);
+  const {num, news, transcript,...restOfComic} = data;
+  const comicToStore = {id, ...restOfComic}
+  await writeJSON(`./comics/${id}`, comicToStore)
 }
